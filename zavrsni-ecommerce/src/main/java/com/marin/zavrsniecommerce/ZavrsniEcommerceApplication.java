60778463c4f61a4a53b51410601895bbcc743be1
Marin <marinjure.jurlina11@gmail.com>
2023-03-10 13:45:49 +0100

    add starter files

---
+++ zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/ZavrsniEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.marin.zavrsniecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZavrsniEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZavrsniEcommerceApplication.class, args);
+	}
+
+}
---
+++ zavrsni-ecommerce/src/test/java/com/marin/zavrsniecommerce/ZavrsniEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marin.zavrsniecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZavrsniEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
