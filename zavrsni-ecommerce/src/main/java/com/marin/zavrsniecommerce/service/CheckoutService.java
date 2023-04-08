f76eae12c20bdf9757faf349bab890dad9f38c36
Marin <marinjure.jurlina11@gmail.com>
2023-04-08 21:13:21 +0200

    add CheckoutService.java

--- zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/service/CheckoutService.java
+++ zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/service/CheckoutService.java
@@ -6,6 +6,7 @@ import com.marin.zavrsniecommerce.dto.PurchaseResponse;
 import com.marin.zavrsniecommerce.entity.Customer;
 import com.marin.zavrsniecommerce.entity.Order;
 import com.marin.zavrsniecommerce.entity.OrderItem;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,7 +21,7 @@ public class CheckoutService {
     private CustomerRepository customerRepository;
 
     @Transactional
-    public PurchaseResponse placeOrder(Purchase purchase){
+    public PurchaseResponse placeOrder(@NotNull Purchase purchase){
 
         Order order = purchase.getOrder();
 
