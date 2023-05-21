ae90217394bd3ad4ace64c6c2acc885843bc78af
Marin <marinjure.jurlina11@gmail.com>
2023-05-22 01:18:51 +0200

    add edit product & order list

--- zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/dao/OrderRepository.java
+++ zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/dao/OrderRepository.java
@@ -1,9 +1,14 @@
 package com.marin.zavrsniecommerce.dao;
 
 import com.marin.zavrsniecommerce.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Page<Order> findByCustomerEmail(@Param("email") String email, Pageable pageable);
 }
--- zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/service/ProductService.java
+++ zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/service/ProductService.java
@@ -3,14 +3,11 @@ package com.marin.zavrsniecommerce.service;
 import com.marin.zavrsniecommerce.dao.ProductRepository;
 import com.marin.zavrsniecommerce.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Local;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 @Service
