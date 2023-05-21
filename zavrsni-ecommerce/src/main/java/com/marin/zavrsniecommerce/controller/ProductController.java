444a1f5bec1c8b41b9736a156236bc86793b9c6b
Marin <marinjure.jurlina11@gmail.com>
2023-05-22 01:07:42 +0200

    add update product

--- zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/controller/ProductController.java
+++ zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/controller/ProductController.java
@@ -4,9 +4,7 @@ import com.marin.zavrsniecommerce.entity.Product;
 import com.marin.zavrsniecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @CrossOrigin("http://localhost:4200")
@@ -19,5 +17,10 @@ public class ProductController {
     @PostMapping("/add-product")
     public Product createNewProduct(@RequestBody Product p){
         return productService.createNewProduct(p);
+    }
+
+    @PutMapping("/edit-product/{id}")
+    public Product editProduct(@PathVariable Long id, @RequestBody Product product){
+        return productService.editProduct(product, id);
     }
 }
--- zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/service/ProductService.java
+++ zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/service/ProductService.java
@@ -3,8 +3,15 @@ package com.marin.zavrsniecommerce.service;
 import com.marin.zavrsniecommerce.dao.ProductRepository;
 import com.marin.zavrsniecommerce.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Service
 @CrossOrigin("http://localhost:4200")
@@ -16,5 +23,22 @@ public class ProductService {
     public Product createNewProduct(Product p) {
         Product saveProduct = productRepository.save(p);
         return saveProduct;
+    }
+
+
+    public Product editProduct(Product p, Long id) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product not found with id: " + id));
+        product.setSku(p.getSku());
+        product.setName(p.getName());
+        product.setDescription(p.getDescription());
+        product.setUnitPrice(p.getUnitPrice());
+        product.setImageUrl(p.getImageUrl());
+        product.setActive(p.isActive());
+        product.setUnitsInStock(p.getUnitsInStock());
+        product.setLastUpdated(Date.from(Instant.now()));
+        product.setCategory(p.getCategory());
+
+        productRepository.save(product);
+        return product;
     }
 }
