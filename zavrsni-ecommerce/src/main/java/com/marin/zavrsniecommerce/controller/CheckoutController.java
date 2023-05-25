880c2dba189631c05a27f44ded25abf05acae3aa
Marin <marinjure.jurlina11@gmail.com>
2023-05-26 00:54:01 +0200

    add stripe backend

--- zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/controller/CheckoutController.java
+++ zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/controller/CheckoutController.java
@@ -1,9 +1,14 @@
 package com.marin.zavrsniecommerce.controller;
 
+import com.marin.zavrsniecommerce.dto.PaymentInfo;
 import com.marin.zavrsniecommerce.dto.Purchase;
 import com.marin.zavrsniecommerce.dto.PurchaseResponse;
 import com.marin.zavrsniecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:4200")
@@ -17,5 +22,13 @@ public class CheckoutController {
     @PostMapping("/purchase")
     private PurchaseResponse placeOrder(@RequestBody Purchase purchase){
         return checkoutService.placeOrder(purchase);
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException{
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+
+        String paymentStr = paymentIntent.toJson();
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
--- zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/service/CheckoutService.java
+++ zavrsni-ecommerce/src/main/java/com/marin/zavrsniecommerce/service/CheckoutService.java
@@ -1,24 +1,32 @@
 package com.marin.zavrsniecommerce.service;
 
 import com.marin.zavrsniecommerce.dao.CustomerRepository;
+import com.marin.zavrsniecommerce.dto.PaymentInfo;
 import com.marin.zavrsniecommerce.dto.Purchase;
 import com.marin.zavrsniecommerce.dto.PurchaseResponse;
 import com.marin.zavrsniecommerce.entity.Customer;
 import com.marin.zavrsniecommerce.entity.Order;
 import com.marin.zavrsniecommerce.entity.OrderItem;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class CheckoutService {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    public CheckoutService(@Value("${stripe.key.secret}")String secretKey){
+        Stripe.apiKey = secretKey;
+    }
 
     @Transactional
     public PurchaseResponse placeOrder(@NotNull Purchase purchase){
@@ -45,5 +53,18 @@ public class CheckoutService {
 
     private String generateOrderTrackingNumber() {
         return UUID.randomUUID().toString();
+    }
+
+    public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException{
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfo.getAmount());
+        params.put("currency", paymentInfo.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+        params.put("receipt_email", paymentInfo.getReceiptEmail());
+
+        return PaymentIntent.create(params);
     }
 }
